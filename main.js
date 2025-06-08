14c09f3b75e4fd630fad47516a4868d742401f8e
icraigshaw <icraigshaw@me.com>
2025-06-08 12:09:23 +0800

    Add entrypoint that opens index.html

---
+++ main.js
@@ -0,0 +1,7 @@
+function main() {
+  // Open the plugin's main UI window when activated
+  // plugin.openWindow opens the given HTML file in Eagle
+  plugin.openWindow('index.html');
+}
+
+module.exports = { main };
